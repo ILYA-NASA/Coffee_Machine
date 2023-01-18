2f5980461d5274d72b215c43d51c6e34f3ad0481
ILYA.NASA <il.nasa@outlook.com>
2023-01-18 09:48:36 +0300

    Stage 2 task is done

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,13 +1,23 @@
 package machine
 
 fun main() {
+//    println(
+//        "Starting to make a coffee\n" +
+//                "Grinding coffee beans\n" +
+//                "Boiling water\n" +
+//                "Mixing boiled water with crushed coffee beans\n" +
+//                "Pouring coffee into the cup\n" +
+//                "Pouring some milk into the cup\n" +
+//                "Coffee is ready!"
+//    )
+
+    println("Write how many cups of coffee you will need:")
+    val input = readln().toInt()
     println(
-        "Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!"
+        "For $input cups of coffee you will need:\n" +
+                input * 200 + " ml of water\n" +
+                input * 50 + " ml of milk\n" +
+                input * 15 + " g of coffee beans"
     )
+
 }
