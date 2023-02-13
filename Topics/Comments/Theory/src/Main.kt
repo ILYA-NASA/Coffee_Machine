a7bf029f59012f02f315315c32a3cf15616dad10
ILYA.NASA <il.nasa@outlook.com>
2023-02-13 20:14:29 +0300

    ENUM classes

--- Topics/Comments/Theory/src/Main.kt
+++ Topics/Comments/Theory/src/Main.kt
@@ -2,4 +2,27 @@
 
 fun main(args: Array<String>) {
   // put your code here
+    println(findByRgb("#FF0000"))
+}
+
+enum class Rainbow(val color: String, val rgb: String) {
+    RED("Red", "#FF0000"),
+    ORANGE("Orange", "#FF7F00"),
+    YELLOW("Yellow", "#FFFF00"),
+    GREEN("Green", "#00FF00"),
+    BLUE("Blue", "#0000FF"),
+    INDIGO("Indigo", "#4B0082"),
+    VIOLET("Violet", "#8B00FF"),
+    NULL("", "");
+
+    fun printFullInfo() {
+        println("Color - $color, rgb - $rgb")
+    }
+}
+
+fun findByRgb(rgb: String): Rainbow {
+    for (rainbow in Rainbow.values()) {
+        if (rgb == rainbow.rgb) return rainbow
+    }
+    return Rainbow.NULL
 }
