a17bd340cec129b81885d344356a016271700d3b
ILYA.NASA <il.nasa@outlook.com>
2023-02-15 12:27:02 +0300

    used setter and comment for coerceIn method

---
+++ Topics/Property accessors/City population/src/Main.kt
@@ -0,0 +1,17 @@
+const val MAX_POPULATION = 50_000_000
+
+data class City(val name: String) {
+    var population: Int = 0
+        set(value) {
+            field = when {
+                value < 0 -> population
+                value > MAX_POPULATION -> MAX_POPULATION
+                else -> value
+            }
+        }
+
+//        set(value) {
+//            field = value.coerceIn(0, 50_000_000)
+//        }
+
+}
