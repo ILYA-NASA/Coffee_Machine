74c2e84868221995f1ebc87ab6bc762c34e78283
ILYA.NASA <il.nasa@outlook.com>
2023-02-17 22:29:29 +0300

    very well solution task

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,137 +1,118 @@
 package machine
 
-var allWater = 400
-var allMilk = 540
-var allCoffeeBeans = 120
-var allCups = 9
-var allMoney = 550
+import java.util.Scanner
 
 fun main() {
-    println("\nWrite action (buy, fill, take, remaining, exit):")
-    val userInput = readln()
-    if (userInput == "exit") return
-    else CoffeeMachine(userInput).controller()
+    val machineContents = MachineContents(400,540,120,9,550)
+    val myFreshCoffeeMachine = CoffeeMachine(machineContents)
+    myFreshCoffeeMachine.runMachine()
 }
 
-class CoffeeMachine(val input: String) {
+data class MachineContents(var water: Int, var milk: Int, var coffeeBeans: Int, var cups: Int, var money: Int)
 
-    fun controller() = when (input) {
-        "buy" -> {
-            println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-            buy(readln())
-        }
+enum class CoffeeTypes(val coffeeName: String, val water: Int, val milk: Int, val coffeeBeans: Int, val cups: Int, val money: Int) {
+    ESPRESSO("Espresso", 250, 0, 16, 1, 4),
+    LATTE("Latte", 350, 75, 20, 1, 7),
+    CAPPUCCINO("Cappuccino", 200, 100, 12, 1, 6);
 
-        "fill" -> {
-            println("Write how many ml of water you want to add:")
-            val water = readln().toInt()
-            println("Write how many ml of milk you want to add:")
-            val milk = readln().toInt()
-            println("Write how many grams of coffee beans you want to add:")
-            val coffeeBeans = readln().toInt()
-            println("Write how many disposable cups you want to add:")
-            val disposableCups = readln().toInt()
-            fill(water, milk, coffeeBeans, disposableCups)
-        }
-
-        "take" -> take()
-        "remaining" -> remaining()
-        else -> main()
-    }
-
-
-    fun remaining() {
-        println(
-            "\nThe coffee machine has:\n" +
-                    "$allWater ml of water\n" +
-                    "$allMilk ml of milk\n" +
-                    "$allCoffeeBeans g of coffee beans\n" +
-                    "$allCups disposable cups\n" +
-                    "$$allMoney of money\n"
-        )
-        main()
-    }
-
-    fun buy(input: String) {
-        when (input) {
-            "1" -> {
-                if (checkingStockLevels(input)) {
-                    allWater -= 250
-                    allCoffeeBeans -= 16
-                    allCups -= 1
-                    allMoney += 4
-                    println("I have enough resources, making you a coffee!")
-                    main()
-                } else {
-                    println("Sorry, not enough water!")
-                    main()
-                }
+    fun buyCoffee(ingredients: MachineContents): MachineContents {
+        when {
+            ingredients.water < this.water -> println("Sorry, not enough water")
+            ingredients.milk < this.milk -> println("Sorry, not enough milk")
+            ingredients.coffeeBeans < this.coffeeBeans -> println("Sorry, not enough coffee beans")
+            ingredients.cups < this.cups -> println("Sorry, not enough cups")
+            else -> {
+                println("I have enough resources, making you a coffee")
+                ingredients.coffeeBeans -= this.coffeeBeans
+                ingredients.water -= this.water
+                ingredients.milk -= this.milk
+                ingredients.cups -= this.cups
+                ingredients.money += this.money
             }
+        }
+        return ingredients
+    }
+}
 
-            "2" -> {
-                if (checkingStockLevels(input)) {
-                    allWater -= 350
-                    allMilk -= 75
-                    allCoffeeBeans -= 20
-                    allCups -= 1
-                    allMoney += 7
-                    println("I have enough resources, making you a coffee!")
-                    main()
-                } else {
-                    println("Sorry, not enough water!")
-                    main()
-                }
+enum class MachineStatus(val description: String) {
+    ACTION("choosing an action"),
+    BUY("choosing a variant of coffee"),
+    INGREDIENTS("displaying ingredient status"),
+    FILL("filling machine with ingredients"),
+    TAKE("taking money from machine"),
+    EXIT("switch off machine"),
+    ERROR("unknown action")
+}
+
+class CoffeeMachine(var machineContents: MachineContents) {
+
+    var status: MachineStatus = MachineStatus.ACTION
+    var scanner = Scanner(System.`in`)
+
+    fun runMachine() {
+        while (status != MachineStatus.EXIT) {
+            when (status) {
+                MachineStatus.ACTION -> this.action()
+                MachineStatus.BUY -> this.buy()
+                MachineStatus.FILL -> this.fill()
+                MachineStatus.INGREDIENTS -> this.ingredientStatus()
+                MachineStatus.TAKE -> this.take()
+                else -> println("Invalid Input")
             }
-
-            "3" -> {
-                if (checkingStockLevels(input)) {
-                    allWater -= 200
-                    allMilk -= 100
-                    allCoffeeBeans -= 12
-                    allCups -= 1
-                    allMoney += 6
-                    println("I have enough resources, making you a coffee!")
-                    main()
-                } else {
-                    println("Sorry, not enough water!")
-                    main()
-                }
-            }
-
-            "back" -> main()
         }
     }
 
-    fun checkingStockLevels(input: String): Boolean {
-        return (when (input) {
-            "1" -> (allWater - 250) >= 0 &&
-                    (allCoffeeBeans - 16) >= 0 &&
-                    (allCups - 1) >= 0
-
-            "2" -> (allWater - 350) >= 0 &&
-                    (allMilk - 75) >= 0 &&
-                    (allCoffeeBeans - 20) >= 0 &&
-                    (allCups - 1) >= 0
-
-            else -> (allWater - 200) >= 0 &&
-                    (allMilk - 100) >= 0 &&
-                    (allCoffeeBeans - 12) >= 0 &&
-                    (allCups - 1) >= 0
+    private fun buy() {
+        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+        val choice = scanner.next()
+        this.status = MachineStatus.ACTION
+        machineContents = when (choice) {
+            "1" -> CoffeeTypes.ESPRESSO.buyCoffee(this.machineContents)
+            "2" -> CoffeeTypes.LATTE.buyCoffee(this.machineContents)
+            "3" -> CoffeeTypes.CAPPUCCINO.buyCoffee(this.machineContents)
+            else -> return
         }
-                )
     }
 
-    fun fill(water: Int, milk: Int, coffeeBeans: Int, disposableCups: Int) {
-        allWater += water
-        allMilk += milk
-        allCoffeeBeans += coffeeBeans
-        allCups += disposableCups
-        main()
+    private fun action() {
+        println("Write action (buy, fill, take, remaining, exit): ")
+        val choice = scanner.next()
+        this.status = when (choice) {
+            "buy" -> MachineStatus.BUY
+            "fill" -> MachineStatus.FILL
+            "take" -> MachineStatus.TAKE
+            "remaining" -> MachineStatus.INGREDIENTS
+            "exit" -> MachineStatus.EXIT
+            else -> MachineStatus.ERROR
+        }
     }
 
-    fun take() {
-        println("I gave you $$allMoney")
-        allMoney = 0
-        main()
+    private fun ingredientStatus() {
+        println("The coffee machine has:")
+        println("${machineContents.water} of water")
+        println("${machineContents.milk} of milk")
+        println("${machineContents.coffeeBeans} of coffee beans")
+        println("${machineContents.cups} of disposable cups")
+        println("${machineContents.money} of money")
+        this.status = MachineStatus.ACTION
+    }
+
+    private fun take() {
+        println("I gave you $${machineContents.money}")
+        machineContents.money = 0
+        this.status = MachineStatus.ACTION
+    }
+
+    private fun fill() {
+        println("Write how many ml of water do you want to add: ")
+        machineContents.water += scanner.nextInt()
+        println("Write how many ml of milk do you want to add: ")
+        machineContents.milk += scanner.nextInt()
+        println("Write how many grams of coffee beans do you want to add: ")
+        machineContents.coffeeBeans += scanner.nextInt()
+        println("Write how many disposable cups of coffee do you want to add: ")
+        machineContents.cups += scanner.nextInt()
+        this.status = MachineStatus.ACTION
     }
 
 }
